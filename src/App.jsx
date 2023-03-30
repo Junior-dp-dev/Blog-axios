dffaa3d57f7a0941f97cca8b1cbb08a899f9375c
Junior <junior.dp.dev@gmail.com>
2023-03-29 22:57:06 -0300

    Primeiros ajustes no projeto
    Implementação do React Router DOM

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1>React Vite</h1>
+      <Navbar />
+      <div className="container">
+        <Outlet />
+      </div>
     </div>
   );
 }
