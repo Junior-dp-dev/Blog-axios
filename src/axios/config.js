f0396d482f46b24827e4bb24a8a7236797c5e701
Junior <junior.dp.dev@gmail.com>
2023-04-06 18:10:55 -0300

    Final do blog
    Adicionado get e post do blog, ficou inacabado, o importante era usar o axios, e o React Router Dom

---
+++ src/axios/config.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+const blogFetch = axios.create({ baseURL: "https://jsonplaceholder.typicode.com", headers: { "Content-Type": "application/json" } });
+
+export default blogFetch;
--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,7 +1,43 @@
 import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./Home.css";
+import blogFetch from "../axios/config";
 
 const Home = () => {
-  return <div>Home</div>;
+  const [posts, setPost] = useState([]);
+
+  const getPost = async () => {
+    try {
+      const response = await blogFetch.get("/posts");
+      setPost(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPost();
+  }, []);
+
+  return (
+    <div className="home">
+      <h1>Últimos posts</h1>
+      {posts.length === 0 ? (
+        <p>Carregando..</p>
+      ) : (
+        posts.map((post) => (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <Link to={`/posts/${post.id}`} className="btn">
+              Ler mais
+            </Link>
+          </div>
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Home;
